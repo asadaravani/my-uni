ecb9529ef4c353f7286f6c82dafff41fa812bd2a
asadaravani <abegano1@stu.vistula.edu.pl>
2025-01-02 21:41:46 +0100

    test CD

--- src/main/java/pl/beganov/myuni/constants/UsosEndpointConstants.java
+++ src/main/java/pl/beganov/myuni/constants/UsosEndpointConstants.java
@@ -7,7 +7,7 @@ public class UsosEndpointConstants {
     private static final String SCOPES = "?scopes=studies|offline_access|email";
     private static final String TT_PARAMS = "?start=2024-12-17&days=1";
     private static final String TT_FIELD_PARAMS = "&fields=course_id" +
-            "|start_time|end_time|lecturer_ids|room_number|classtype_name|group_number";
+            "|start_time|end_time|lecturer_ids|room_number|classtype_name|group_number|sex";
     private static final String USER_INFO_PARAMS = "?fields=id|first_name|last_name|student_status|student_programmes|course_editions_conducted";
 
     public static final String REQUEST_TOKEN_URL = AUTH_URL + "/request_token" + SCOPES;
