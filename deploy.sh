d4f1afea9d6d4afa5711162abff0a4a2553dd4a8
asadaravani <abegano1@stu.vistula.edu.pl>
2025-01-02 21:23:52 +0100

    add deploy.sh file

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Stopping existing application..."
+pkill -f 'java -jar'
+
+echo "Pulling latest changes from GitHub..."
+git pull origin main
+
+echo "Building the application..."
+./mvnw clean install
+
+echo "Starting the application..."
+nohup java -jar target/my-uni-0.0.1-SNAPSHOT.jar > app.log 2>&1 &
+echo "Application deployed successfully!"
